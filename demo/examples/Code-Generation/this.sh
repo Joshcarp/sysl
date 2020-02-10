8793d001e3ac0053dfc65a9bd1427cbdc94e6f4b
joshcarp <joshuacarpeggiani@gmail.com>
2020-02-11 09:44:24 +1100

    Update makefile

--- demo/examples/Code-Generation/this.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e ;\
-if [ ! -d ".tmp/server-lib" ]; then \
-    git clone https://github.service.anz/sysl/server-lib/ .tmp/server-lib \;
-fi
-cd  .tmp/server-lib  && git fetch && git pull && cd ../..
