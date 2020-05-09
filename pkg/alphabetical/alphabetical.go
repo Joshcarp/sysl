3d6cc7fbef9b1fcde11fdca04b7081c7b79aa706
joshcarp <joshuacarpeggiani@gmail.com>
2020-05-10 00:03:22 +1000

    Add Alphabetical funcs

---
+++ pkg/alphabetical/alphabetical.go
@@ -0,0 +1,71 @@
+package alphabetical
+
+import (
+	"github.com/anz-bank/sysl/pkg/sysl"
+	"sort"
+)
+
+func Attribute(m map[string]*sysl.Attribute) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func Module(m map[string]*sysl.Module) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func Application(m map[string]*sysl.Application) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func Endpoint(m map[string]*sysl.Endpoint) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		if k != "" {
+			keys = append(keys, k)
+		}
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func Type(m map[string]*sysl.Type) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func Int64(m map[string]int64) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func Param(m map[string]*sysl.Param) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
